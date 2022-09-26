29850f8d6fdc7cfb49d1a99e91500d1eb6bcd1e4
Robert Teets <teetsrobert@gmail.com>
2022-09-26 12:27:27 -0600

    3: finished assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import PersonCard from './PersonCard'
-import './App.css';
+import PersonCard from './components/PersonCard'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <PersonCard />
+      <PersonCard firstName='Robert' lastName='Teets' age={ 25 } hairColor='Brown' />
+      <PersonCard firstName='Jane' lastName='Doe' age={ 45 } hairColor='Black' />
+      <PersonCard firstName='John' lastName='Smith' age={ 88 } hairColor='Brown' />
+      <PersonCard firstName='Millard' lastName='Fillmore' age={ 50 } hairColor='Blonde' />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -4,9 +4,11 @@ const PersonCard = props => {
     const { firstName, lastName, age, hairColor } = props
     return (
         <>
-            <h2>{ lastName } { firstName }</h2>
+            <h2>{ lastName }, { firstName }</h2>
             <p>Age: { age }</p>
             <p>Hair Color: { hairColor }</p>
         </>
     )
 }
+
+export default PersonCard
