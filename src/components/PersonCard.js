a747fc24220954b2bcd454224c78a32458a7af4b
Robert Teets <teetsrobert@gmail.com>
2022-09-26 12:21:27 -0600

    2: created PersonCard component

---
+++ src/components/PersonCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PersonCard = props => {
+    const { firstName, lastName, age, hairColor } = props
+    return (
+        <>
+            <h2>{ lastName } { firstName }</h2>
+            <p>Age: { age }</p>
+            <p>Hair Color: { hairColor }</p>
+        </>
+    )
+}
